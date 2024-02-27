0eefc445ae3a9f2a311b5dceddb893fdab4b868d
David Johan Hasiholan Parhusip <david.johanhp@gmail.com>
2024-02-27 19:14:09 +0700

    remove github token

--- app/src/main/java/com/example/proyekgithubuser/ApiService.kt
+++ app/src/main/java/com/example/proyekgithubuser/ApiService.kt
@@ -6,25 +6,25 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token //insert TOKEN HERE")
     fun findUserByUsername(
         @Query("q") username: String
     ): Call<GithubResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token //insert TOKEN HERE")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token //insert TOKEN HERE")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<UsersItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token //insert TOKEN HERE")
     fun getFollowing(
         @Path("username") username: String
     ): Call<List<UsersItem>>
